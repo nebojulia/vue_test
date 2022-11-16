4b216d12e09a2b59233afd52d2bcbbb24ffd5fd4
Julia <nebo.julia@gmail.com>
2022-11-16 18:47:36 +0300

    vue start

---
+++ src/components/PostForm.vue
@@ -0,0 +1,50 @@
+<template>
+    <form @submit.prevent>
+        <h4>Создание поста</h4>
+        <input 
+            v-bind:value="title" 
+            @input="title = $event.target.value"
+            class="input" 
+            type="text" 
+            placeholder="Название"
+        >
+        <input 
+            v-bind:value="body"
+            @input="body = $event.target.value" 
+            class="input" 
+            type="text" 
+            placeholder="Описание"
+        >
+        <button 
+            class="btn" 
+            >Создать
+        </button>
+    </form>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.input {
+    width: 100%;
+    border: 1px solid teal;
+    padding: 10px 15px;
+    margin-top: 15px;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: end;
+    padding: 10px 15px;
+    background: none;
+    color: teal;
+    border: 1px solid teal;
+}
+</style>
